c012915e3adabe1570c2b64cb404af5f005ac1d4
vajda <eib.kapitan@gmail.com>
2022-06-04 09:35:54 +0200

    skusal som pridat loader, stale neuspesne

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ const loader = document.querySelector(.loader);
 const video = document.querySelector(.video);
 
 function init(){
-    setTimeout(() =>
-    
-    )
+    setTimeout(() => {
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
 
-    
+        main.style.display = 'block';
+        setTimeout(() => (main.style.opacity = 1),50); 
+    },4000);
 }
 
 init();
